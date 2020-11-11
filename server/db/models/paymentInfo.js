f8846ce0ea445fd38dc2ced51ad991c4f0fed988
Yael Bassal <ybassal@hotmail.com>
2020-11-11 07:38:53 -0500

    working on paymentInfo model and test specs

--- server/db/models/paymentInfo.js
+++ server/db/models/paymentInfo.js
@@ -6,9 +6,11 @@ const PaymentInfo = db.define('paymentInfo', {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
+      //cannot be an empty string
       notEmpty: true,
     }
   },
+  //made this field optional since not all folks have a middle name.
   middleInitial: {
     type: Sequelize.STRING,
   },
@@ -16,6 +18,7 @@ const PaymentInfo = db.define('paymentInfo', {
     type: Sequelize.STRING,
     allowNull: false,
     validate:{
+      //cannot be an empty string
       notEmpty: true,
     }
   },
@@ -28,10 +31,14 @@ const PaymentInfo = db.define('paymentInfo', {
     }
   },
   cardExpirationDate: {
-    type: Sequelize.DATE,
+    //date documentation, link: https://sequelize.org/v5/. An alternative could be to use a string datatype.
+    //DATEONLY gives us date w/o time.
+    type: Sequelize.DATEONLY,
     validate: {
-      //TODAY'S DATE
-      isAfter:
+      //checks if input is a date
+      isDate: true,
+      //today's date
+      isAfter: "2020-11-11",
     }
 
   },
@@ -42,7 +49,7 @@ const PaymentInfo = db.define('paymentInfo', {
       isNumeric: true,
       //isThreeDigits is a custom validator used to check if a user inputs a cvv of a correct length of 3 or 4 digits. Documentation: https://sequelize.org/master/manual/validations-and-constraints.html
       //alternative can be len: [3,4] or a variation of this.
-      isThreeDigits(value) {
+      isThreeOrFourDigits(value) {
         if (value.length !== 3 || value.length !== 4) {
           throw new Error('A cvv can only be three or four digits long.')
         }
@@ -54,7 +61,10 @@ const PaymentInfo = db.define('paymentInfo', {
     allowNull: false
   },
   zipCode: {
+    //VARCHAR is also a datatype we could use for zipcodes. I left it as string for "global" users. We can also do VARCHAR(5) for just United States zipcodes.
     type: Sequelize.STRING,
     allowNull: false
   }
 })
+
+module.exports = PaymentInfo
