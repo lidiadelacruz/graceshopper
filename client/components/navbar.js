364bdd1dc74c2cffdb5b4d04a924c4f53debe77d
Carolyn Berard <carolynmberard@gmail.com>
2020-11-14 15:39:18 -0600

    Feature: Web Design: In Progress

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,25 +7,29 @@ import {logout} from '../store'
 const Navbar = ({handleClick, isLoggedIn = false}) => {
   return (
     <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        )}
+      <nav className="navbar">
+        <div className="logo">
+          <img src="nbh-logo.png" alt="Never Before Homes Logo" />
+          <h1>BOILERMAKER</h1>
+        </div>
+        <div className="nav-right">
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          )}
+        </div>
       </nav>
-      <hr />
     </div>
   )
 }
